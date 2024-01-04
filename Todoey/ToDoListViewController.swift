0b5be66b3ee49dda79de0d29d644d6904a2881f5
Ajay Kumar <124868129+ajay-kumar-iphtech@users.noreply.github.com>
2024-01-04 23:01:01 +0530

    initial-commit

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    var array = ["ajay", "aman", "sachin", "pradeep"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = array[indexPath.row]
+        return cell
+        
+        
+    }
+    
+}
+
