09daa9ebeb4fe65d1a8eacb88be38296d0ad09e1
Ajay Kumar <124868129+ajay-kumar-iphtech@users.noreply.github.com>
2024-01-04 23:25:04 +0530

    add tableView accessoryType

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Todoey
-//
-//  Created by Philipp Muellauer on 02/12/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
 
 import UIKit
 
@@ -14,7 +7,7 @@ class ToDoListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,9 +19,15 @@ class ToDoListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = array[indexPath.row]
         return cell
-        
-        
     }
+ 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+           if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+               tableView.cellForRow(at: indexPath)?.accessoryType = .none
+           } else {
+               tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+           }
+       }
     
 }
 
